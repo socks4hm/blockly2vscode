da06dbd320531658bfb7598abec33decde456a91
socks4hm <socks4penguins@hotmail.com>
2021-07-23 03:40:51 +0000

    blockly generating code

--- src/features/home/BlocklyTest.js
+++ src/features/home/BlocklyTest.js
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useBlocklyWorkspace } from 'react-blockly';
 import ConfigFiles from '../../data/content';
+import Blockly from 'blockly';
 
 // import PropTypes from 'prop-types';
 
@@ -9,14 +10,28 @@ export default function BlocklyTest() {
   const [toolboxConfiguration, setToolboxConfiguration] = React.useState(
     ConfigFiles.INITIAL_TOOLBOX_JSON,
   );
+  const [code, setCode] = useState(null);
+
   const { workspace, xml } = useBlocklyWorkspace({
     ref: blocklyRef,
     toolboxConfiguration: toolboxConfiguration, // this must be a JSON toolbox definition
     initialXml: ConfigFiles.INITIAL_XML,
+    onWorkspaceChange: handleWorkspaceChange,
   });
+
+  function handleWorkspaceChange(data) {
+    setCode(Blockly.JavaScript.workspaceToCode(workspace));
+  }
+
+  // eslint-disable-next-line no-undef
+  //  Blockly.JavaScript.workspaceToCode(workspace);
   return (
     <div className="home-blockly-test fit vertical layout">
+      <div>blockly test</div>
       <div className="flex" ref={blocklyRef} />
+      <div className="horizontal layout" style={{ height: '20%' }}>
+        <div id="code">{code}</div>
+      </div>
     </div>
   );
 }
